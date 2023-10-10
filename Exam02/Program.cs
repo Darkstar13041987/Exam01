1a0fe42c3724a84c37060f234e0823abfeb3db36
Mamykin <mamykin_87@mail.ru>
2023-10-10 20:41:02 +0300

    Exam02

---
+++ Exam02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя:");
+string name = Console.ReadLine();
+Console.WriteLine("Hello ");
+Console.WriteLine(name);
+
+
+
+// Console.WriteLine("Введите имя:");
+// string name = Console.ReadLine();
+// Console.WriteLine("Привет,");
+// Console.WriteLine(name);
