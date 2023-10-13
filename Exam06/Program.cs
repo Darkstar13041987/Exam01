3084326b7211a542b7def9ac9499266accd92939
Mamykin <mamykin_87@mail.ru>
2023-10-13 15:15:51 +0300

    Exam06

---
+++ Exam06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if(username == "Masha")       // .ToLover
+{
+    Console.WriteLine("Ура,привет Маша");    
+}
+else
+{
+  Console.Write("Привет, ");  
+  Console.WriteLine(username);
+}
